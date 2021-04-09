b20dbac4fcebafc4b5836268a1e47c575df943fe
Mike Altebrando <m.j.altebrando@gmail.com>
2021-04-09 11:51:46 -0400

    Added Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BackendService} from './backend.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
